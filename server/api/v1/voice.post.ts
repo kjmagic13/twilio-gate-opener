2daf659b42406305c152de9178ae734890efc9b4
Kevin <kjmagic13@gmail.com>
2024-03-26 15:16:08 -0400

    init twilio, test endpoint

---
+++ server/api/v1/voice.post.ts
@@ -0,0 +1,21 @@
+import VoiceResponse from "twilio/lib/twiml/VoiceResponse.js";
+
+export default defineEventHandler(async (event) => {
+  setResponseHeaders(event, {
+    "Content-Type": "text/xml",
+  });
+
+  const body = await readBody(event);
+
+  console.log("***************************");
+  console.log("***************************");
+  console.log("***************************");
+  console.log(body);
+
+  const twiml = new VoiceResponse();
+
+  twiml.say("Welcome!");
+  twiml.dial("9");
+
+  return twiml.toString();
+});
