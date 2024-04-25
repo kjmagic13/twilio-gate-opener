9eec83b4f71674f8d16bbbc9817c0ed38c436c81
Kevin <kjmagic13@gmail.com>
2024-04-25 18:15:35 -0400

    remove cowbell

--- server/api/v1/voice.post.ts
+++ server/api/v1/voice.post.ts
@@ -15,7 +15,7 @@ export default defineEventHandler(async (event) => {
   const twiml = new VoiceResponse();
 
   twiml.say("Welcome!");
-  twiml.play({ digits: "9" }, "https://api.twilio.com/cowbell.mp3");
+  twiml.play({ digits: "9" });
   twiml.say("Please enter now!");
 
   twiml.hangup();
